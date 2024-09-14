de5751ef1c23a2aff39020ebb60fe0d9083710a2
escobardie <diego_escobar@live.com>
2024-09-14 16:16:15 -0300

    ADD datos
    SE AGREGO DATO DENTRO DE LA BASE DE DATOS.-

--- carga_de_datos_prueba.sql
+++ carga_de_datos_prueba.sql
@@ -5,21 +5,22 @@ SELECT * FROM cliente_visita;
 SELECT * FROM cliente_registropago;
 
 
-INSERT INTO cliente_promo ( Nombre_Promo, Valor_Promo, Cant_bidones, Alta_promo, Vencimiento_promo, Estado, Nota)
-VALUES ( 'Dispenser (SIN BIDONES)', 21000.00, 0, '2024-09-01', '2025-12-31', TRUE, 'Promoción válida para nuevos clientes.');
+-- Borrar los datos de la tabla
+DELETE FROM cliente_promo;
 
-INSERT INTO cliente_cliente (Tipo_Promo_id, Nombre, Apellido, Telefono, Direccion, Fecha_alta, Fecha_Cobro, Estado)
-VALUES (2, 'Diego', 'Escobar', '154078790', 'Corrientes 2080', '2024-09-01', '2024-09-29', TRUE);
--- Nota: En este ejemplo, Tipo_Promo es 1, que corresponde a la primera promoción que insertamos en la tabla PROMO.
+-- Reiniciar el contador de AUTO_INCREMENT a 1
+ALTER TABLE cliente_registropago AUTO_INCREMENT = 1;
 
-INSERT INTO cliente_promoporcliente (cliente_id, promo_id, Inicio_promo, Fin_promo, Bidones_disponibles, Codigo_dispenser, Estado, Nota)
-VALUES (2, 2, '2024-08-29', '2025-02-28', 10, 'DISP001', TRUE, 'Cliente nuevo con promoción verano');
--- Nota: En este caso, ID_Cliente es 1 y ID_Promo es 1, que corresponden a los registros previamente insertados en las tablas CLIENTE y PROMO.
 
-INSERT INTO cliente_visita (cliente_id, Fecha_visita, Nota)
-VALUES (1, '2024-09-01', 'Primera visita de instalación de dispenser.');
--- Nota: ID_Cliente en ambas tablas se refiere al cliente con ID igual a 1, que es el primer cliente que insertamos.
+-- Inserta un nuevo registro en la tabla cliente_promo
+INSERT INTO cliente_promo (nombre_promo, valor_promo, cant_bidones, alta_promo, vencimiento_promo, estado, nota)
+VALUES ('DISPENSER CON 6 BIDONES', 21000.00, 6, '2024-09-01', '2024-12-31', 1, 'Promoción válida para compras durante el verano.');
 
-INSERT INTO cliente_registropago (cliente_id, Fecha, Monto, Comprobante, Nota)
-VALUES (1, '2024-09-01', 15000.00, 'COM001', 'Pago en efectivo.');
--- Nota: ID_Cliente en ambas tablas se refiere al cliente con ID igual a 1, que es el primer cliente que insertamos.
+INSERT INTO cliente_promo (nombre_promo, valor_promo, cant_bidones, alta_promo, vencimiento_promo, estado, nota)
+VALUES ('DISPENSER CON 10 BIDONES', 22000.00, 10, '2024-09-01', '2024-12-31', 1, 'Promoción válida para compras durante el verano.');
+
+INSERT INTO cliente_promo (nombre_promo, valor_promo, cant_bidones, alta_promo, vencimiento_promo, estado, nota)
+VALUES ('DISPENSER CON 4 BIDONES', 20000.00, 4, '2024-09-01', '2024-12-31', 1, 'Promoción válida para compras durante el verano.');
+
+INSERT INTO cliente_promo (nombre_promo, valor_promo, cant_bidones, alta_promo, vencimiento_promo, estado, nota)
+VALUES ('DISPENSER SIN BIDONES', 15000.00, 0, '2024-09-01', '2024-12-31', 1, 'Promoción válida para compras durante el verano.');
