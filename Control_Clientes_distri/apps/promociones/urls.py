8b692fb4a193da16092469d3b43cbe6c8965168a
escobardie <diego_escobar@live.com>
2024-11-28 23:07:59 -0300

    PROMOCIONES
    VIEW PROMOCIONES (LISTAR PROMOCIONES)
    HTML PROMOCIONES

--- Control_Clientes_distri/apps/promociones/urls.py
+++ Control_Clientes_distri/apps/promociones/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('nueva_promo/', views.PromoCreateView.as_view(), name='nueva_promo'),
+    path('listar_promociones/', views.ListarPromocionesView.as_view(), name='listar_promociones'),
 ]
--- Control_Clientes_distri/apps/promociones/views.py
+++ Control_Clientes_distri/apps/promociones/views.py
@@ -1,6 +1,7 @@
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import user_passes_test
 from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 from django.urls import reverse_lazy
 from . import models, forms
 
@@ -11,9 +12,18 @@ def usuario_es_admin(user):
 class PromoCreateView(CreateView):
     model = models.Promo
     form_class = forms.AddPromoForm
-    template_name = 'Agua/forms/crear_promo.html'
-    success_url = reverse_lazy('inicio')
+    template_name = 'Base/forms/crear_promo.html'
+    success_url = reverse_lazy('listar_promociones')
     
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+@method_decorator(user_passes_test(usuario_es_admin, login_url='inicio'), name='dispatch')
+class ListarPromocionesView(ListView):
+    model = models.Promo
+    template_name = "Base/listar_promociones.html"
+    context_object_name = 'lista_promos'
+    paginate_by = 5
+    queryset = models.Promo.objects.filter(estado=True).order_by('nombre_promo')
